8e333047ab2910e77ab9f2cf982b6909874bcf5a
Pavel Krotov <pavelkrotov@Pavels-MBP.home>
2017-04-14 08:25:48 -0400

    first commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,47 @@
-console.log('\'Allo \'Allo!');
+var vm = new Vue({
+    el: '#app',
+    data: {
+      title: '',
+      name: '',
+      needIt: true,
+      sideBar: false,
+      isSelected: false,
+      selectedItem: {},
+      startSearch: '',
+      searchOutput: '',
+      url: 'http://www.omdbapi.com/?s=',
+      mvs: []
+    }
+    ,
+    watch: {
+      name: function() {
+        this.searchOutput = '';
+        if(this.name.length > 0) {
+          this.getSome();
+          this.needIt = false;
+        }
+      }
+
+    },
+    methods: {
+      getSome: _.debounce(function () {
+        axios.get('http://www.omdbapi.com/?s=' + this.name)
+          .then(function (response) {
+            console.log(response)
+              vm.mvs = response.data.Search;
+              vm.searchOutput = vm.mvs.slice(0,7);
+              vm.title = '';
+
+            // else if (vm.name.length > 1){ vm.title = 'Try a different word!' }
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      }, 500),
+      onShowMovie: function (id) {
+        this.selectedItem = id;
+        this.isSelected = true;
+        console.log(this.selectedItem)
+      }
+    }
+  })
